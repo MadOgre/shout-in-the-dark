558836bddad406c5d2737de4c762b2f778b44ada
Mike Semko <msemko@gmail.com>
2016-08-02 21:52:40 -0400

    refactored preview controller again because it apparently did not get included in the previous commit

--- controllers/preview_controller.js
+++ controllers/preview_controller.js
@@ -7,50 +7,65 @@ var getImages = require(__dirname + "/../helpers/get_images.js");
 var memify = require(__dirname + "/../helpers/memify.js");
 
 router.get("/", function(req, res){
-	getImages(req.query.q, function(err, data){
+	var id = new Date().valueOf().toString();
+	var fileName = id + ".png";
+	var imagesData = {};
+	getImages(req.query.q, parseResponse);
+	
+	function parseResponse(err, data) {
 		if (err) {
-				console.error("Error: " + err);
-				res.status(500).send({Error: err});
-		} else {
-			var id = new Date().valueOf().toString();
-			var fileName = id + ".png";
-			var readStream = fs.createReadStream(__dirname + "/../public/img/blank.png");
-			var writeStream = fs.createWriteStream(__dirname + "/../public/img/" + fileName);
-			readStream.pipe(writeStream);
-			writeStream.on("close", function(err){
-				if (err) {
-					console.error("Error: " + err);
-					res.status(500).send({Error: err});
-				} else {
-					memify(__dirname + "/../public/img/" + fileName, req.query.q, function(err){
-						if (err) {
-							console.error("Error: " + err);
-							res.status(500).send({Error: err});
-						} else {
-							var response = {
-								transparency: "/img/" + fileName,
-								images: []
-							};
-							data.forEach(function(item){
-								response.images.push({
-									thumb: item.thumbnail.url,
-									full: item.url
-								});
-							});
-							res.json(response);
-							setTimeout(function(){
-								fs.unlink(__dirname + "/../public/img/" + fileName, function(err){
-									if (err) {
-										return console.error(err);
-									}
-								});
-							}, 5000);
-						}
-					});
-				}
-			});			
+			console.error("Error: " + err);
+			return res.status(500).send({Error: err});
 		}
-	});
+		imagesData = data;
+		var readStream = fs.createReadStream(__dirname + "/../public/img/blank.png");
+		var writeStream = fs.createWriteStream(__dirname + "/../public/img/" + fileName);
+		readStream.pipe(writeStream);
+		writeStream.on("close", generateAndSendResponse);					
+	}
+
+	function generateAndSendResponse(err) {
+		if (err) {
+			console.error("Error: " + err);
+			return res.status(500).send({Error: err});
+		}
+		memify(__dirname + "/../public/img/" + fileName, req.query.q, sendResponse);	
+	}
+
+	function sendResponse(err) {
+		if (err) {
+			console.error("Error: " + err);
+			return res.status(500).send({Error: err});
+		}
+		var response = {
+			transparency: "/img/" + fileName,
+			images: []
+		};
+		imagesData.forEach(function(item){
+			response.images.push({
+				thumb: item.thumbnail.url,
+				full: item.url
+			});
+		});
+		res.json(response);
+		deleteFileTimer(__dirname + "/../public/img/" + fileName, 5000, function(err){
+			if (err) {
+				return console.error(err);
+			}
+		});
+	}
+
+	function deleteFileTimer(path, delay, callback) {
+		setTimeout(function(){
+			fs.unlink(path, function(err){
+				if (err) {
+					callback(err)
+				} else {
+					callback(null)
+				}
+			});
+		}, delay)
+	}	
 });
 
 module.exports = router;

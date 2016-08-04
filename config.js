0be7e9c5da4687a39f483e9769f8cb2dc3f88060
Mike Semko <msemko@gmail.com>
2016-08-04 02:50:43 -0400

    added cors to the dependencies list, changed database path to outsourced database to be shared across dev machines

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	database_url: 'mongodb://localhost/shout',
+	database_url: 'mongodb://madogre:warcraft@ds139665.mlab.com:39665/shout', //'mongodb://localhost/shout',
 	default_transparency_path: process.cwd() + '/public/img/common/transparency.png'
 }

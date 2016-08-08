c829cbb2ee6378f7e9e9c01e9e23567a48bb9e0e
Mike Semko <msemko@gmail.com>
2016-08-08 00:13:48 -0400

    fixed consequences of copy and paste

--- fixtures/mock_data3.js
+++ fixtures/mock_data3.js
@@ -6,55 +6,55 @@ module.exports = [
 		}
 	},
 	{
-		url: 'http://2.bp.blogspot.com/-Li164jKPbsA/T5_dAcOsj6I/AAAAAAAABd8/3Ake9SyEoO8/s1600/ford%2Bcar-2.jpg'
+		url: 'http://2.bp.blogspot.com/-Li164jKPbsA/T5_dAcOsj6I/AAAAAAAABd8/3Ake9SyEoO8/s1600/ford%2Bcar-2.jpg',
 		thumbnail: {
 			url: 'http://ts3.mm.bing.net/th?id=OIP.M94d6aea1979d1cd78928ebd8bd2473a3o0&pid=15.1'
 		}
 	},
 	{
-		url: 'http://3.bp.blogspot.com/-_NHpXcP01rM/T6PAhXNqLtI/AAAAAAAACH4/mP0zcMJlH8I/s1600/black+jaguar+car-.jpg'
+		url: 'http://3.bp.blogspot.com/-_NHpXcP01rM/T6PAhXNqLtI/AAAAAAAACH4/mP0zcMJlH8I/s1600/black+jaguar+car-.jpg',
 		thumbnail: {
 			url: 'http://ts3.mm.bing.net/th?id=OIP.M6fcc7dc1be192f8a82db8ae0e3d57513H0&pid=15.1'
 		}
 	},
 	{
-		url: 'http://autonewsblog.org/wp-content/uploads/2009/06/dreamcar.jpg'
+		url: 'http://autonewsblog.org/wp-content/uploads/2009/06/dreamcar.jpg',
 		thumbnail: {
 			url: 'http://ts1.mm.bing.net/th?id=OIP.M957cfbe93bfc7fd213801bdff152dab1o0&pid=15.1'
 		}
 	},
 	{
-		url: 'http://1.bp.blogspot.com/-aiSvFbaUVLI/TnTkg1up-yI/AAAAAAAAdDY/1SPFouUo3EA/s1600/new+car+pics-1.jpg'
+		url: 'http://1.bp.blogspot.com/-aiSvFbaUVLI/TnTkg1up-yI/AAAAAAAAdDY/1SPFouUo3EA/s1600/new+car+pics-1.jpg',
 		thumbnail: {
 			url: 'http://ts4.mm.bing.net/th?id=OIP.Mb4011a736a8ac9b0f80155a0a0ddb2d3H0&pid=15.1'
 		}
 	},
 	{
-		url: 'http://1.bp.blogspot.com/-2zg_dKpMcmg/TjVZgmtGOFI/AAAAAAAAAAQ/eIzqQHW5QLw/s1600/bmw-cars.jpg'
+		url: 'http://1.bp.blogspot.com/-2zg_dKpMcmg/TjVZgmtGOFI/AAAAAAAAAAQ/eIzqQHW5QLw/s1600/bmw-cars.jpg',
 		thumbnail: {
 			url: 'http://ts4.mm.bing.net/th?id=OIP.Ma70262ce7a93338a56e2edd564a84903H0&pid=15.1'
 		}
 	},
 	{
-		url: 'http://atlantadent.com/wp-content/uploads/2012/11/step_1_white_car.jpg'
+		url: 'http://atlantadent.com/wp-content/uploads/2012/11/step_1_white_car.jpg',
 		thumbnail: {
 			url: 'http://ts2.mm.bing.net/th?id=OIP.M11d1f15e4cf626a865402e6de3d0b8d8o0&pid=15.1'
 		}
 	},
 	{
-		url: 'http://3.bp.blogspot.com/-Resxw6iRBKc/T60Kx4ZRPpI/AAAAAAAAChk/6HhFgB9tvFg/s1600/sports+car+pics-5.jpg'
+		url: 'http://3.bp.blogspot.com/-Resxw6iRBKc/T60Kx4ZRPpI/AAAAAAAAChk/6HhFgB9tvFg/s1600/sports+car+pics-5.jpg',
 		thumbnail: {
 			url: 'http://ts2.mm.bing.net/th?id=OIP.Mb9b6207d50d2f7cec890c7e5eb5d3ce7H0&pid=15.1'
 		}
 	},
 	{
-		url: 'http://1.bp.blogspot.com/-uqRWXuHq-TQ/TcJx_YISOcI/AAAAAAAACec/Ws7377GFo-w/s1600/a-free-wallpaper-depicting-a-luxury-modern-black-car-_0_4.jpg'
+		url: 'http://1.bp.blogspot.com/-uqRWXuHq-TQ/TcJx_YISOcI/AAAAAAAACec/Ws7377GFo-w/s1600/a-free-wallpaper-depicting-a-luxury-modern-black-car-_0_4.jpg',
 		thumbnail: {
 			url: 'http://ts3.mm.bing.net/th?id=OIP.Ma2e53e8e5ac7a74ea87247e0a24dc1f2o0&pid=15.1'
 		}
 	},
 	{
-		url: 'http://px6.streetfire.net/0001/02/85/1072058_600.jpg'
+		url: 'http://px6.streetfire.net/0001/02/85/1072058_600.jpg',
 		thumbnail: {
 			url: 'http://ts2.mm.bing.net/th?id=OIP.M4921df62fee2055f92003821ef90d851o0&pid=15.1'
 		}

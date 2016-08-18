8d19b80ad1a2b82acfe7d67593600e51d2a8f6bc
Mike Semko <msemko@gmail.com>
2016-08-17 22:55:57 -0400

    completed infinite scroll implementation

--- public/js/main.js
+++ public/js/main.js
@@ -8,26 +8,32 @@
 
     vm.currentPage = 0;
 
+    var gettingShouts = false;
+
     vm.nextPage = function() {
       if (vm.shouts.length < 1) return;
       vm.getShouts();
     }
 
     vm.getShouts = function() {
-      $http({
-        method: 'GET',
-        headers: {
-          'Caller': 'angular'
-        },
-        url: '/shout?p=' + (vm.currentPage)
-      }).then(function successCallback(response) {
+      if (!gettingShouts) {
+        gettingShouts = true;
+        $http({
+          method: 'GET',
+          headers: {
+            'Caller': 'angular'
+          },
+          url: '/shout?p=' + (vm.currentPage)
+        }).then(function successCallback(response) {
           vm.currentPage++;
           vm.shouts = vm.shouts.concat(response.data);
-          console.log(vm.shouts);
+          gettingShouts = false;
         }, function errorCallback(response) {
           alert('error loading shouts');
           console.warn(response);
+          gettingShouts = false;
         });
+      }
     }
     vm.getShouts();
   }

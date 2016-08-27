78d1b5a13444902cf0506e03b2f71215522d8db5
Phil Flanagan <phileflanagan@gmail.com>
2016-08-26 20:05:40 -0400

    add frontpage loader

--- public/js/main.js
+++ public/js/main.js
@@ -5,19 +5,17 @@
   function Main($http) {
     var vm = this;
     vm.shouts = [];
-
+    vm.gettingShouts = false;
     vm.currentPage = 0;
-
-    var gettingShouts = false;
-
     vm.nextPage = function() {
       if (vm.shouts.length < 1) return;
       vm.getShouts();
     }
 
     vm.getShouts = function() {
-      if (!gettingShouts) {
-        gettingShouts = true;
+      vm.loading = true;
+      if (!vm.gettingShouts) {
+        vm.gettingShouts = true;
         $http({
           method: 'GET',
           headers: {
@@ -27,11 +25,11 @@
         }).then(function successCallback(response) {
           vm.currentPage++;
           vm.shouts = vm.shouts.concat(response.data);
-          gettingShouts = false;
+          vm.gettingShouts = false;
         }, function errorCallback(response) {
           alert('error loading shouts');
           console.warn(response);
-          gettingShouts = false;
+          vm.gettingShouts = false;
         });
       }
     }
